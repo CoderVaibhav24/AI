014bbd21e29761ddafe25a55883b0a9a123dd0d7
Vaibhav <vsahay717@gmail.com>
2025-07-21 04:12:44 +0530

    Enhance authentication flow by extending token expiration to 7 days, adding onboarding functionality, and implementing route protection middleware.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -50,14 +50,14 @@ export async function signup(req, res) {
     }
 
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "7d",
     });
 
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "strict",
       secure: process.env.NODE_ENV === "production",
-      maxAge: 3600000, // 1 hour
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     await newUser.save();
@@ -93,14 +93,14 @@ export async function login(req, res) {
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "7d",
     });
 
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "strict",
       secure: process.env.NODE_ENV === "production",
-      maxAge: 3600000, // 1 hour
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.status(200).json({ success: true, message: "Login successful" });
@@ -113,4 +113,53 @@ export async function login(req, res) {
 export function logout(req, res) {
   res.clearCookie("token");
   res.status(200).json({ success: true, message: "Logout successful" });
+}
+
+export async function onboard(req, res) {
+  try{
+    const userId = req.user._id;
+    const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
+
+    if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location){
+      return res.status(400).json({ 
+        message: "All fields are required" ,
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ],
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, {
+      ...req.body,
+      isOnboarded: true,
+    }, { new: true });
+
+    if(!updatedUser){
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    try {
+      await upsertStreamClient({
+        id: updatedUser._id.toString(),
+        name: updatedUser.fullName,
+        image: updatedUser.profilePicture || "",
+      });
+      console.log(`Stream client updated successfully for user: ${updatedUser.fullName}`);
+    } catch (error) {
+      console.error("Error updating Stream client:", error);
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User onboarded successfully",
+    });
+
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
